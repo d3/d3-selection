f0eab4fb2f82d3cfaba34434e5e72d3feb27d5e8
Mike Bostock <mbostock@gmail.com>
2015-05-11 13:03:48 -0700

    Expose namespaces as d3.namespace in standalone.
    Since I’m not planning on exporting any methods related to namespaces in the
    standalone API, it seems simpler to just export the map of prefixes.

--- standalone.js
+++ standalone.js
@@ -2,4 +2,4 @@ var d3 = global.d3 || (global.d3 = {}),
     selection = d3.selection = require("./lib/d3/selection");
 d3.select = selection.select;
 d3.selectAll = selection.selectAll;
-d3.namespace = {prefix: require("./lib/d3/namespace/prefix")};
+d3.namespace = require("./lib/d3/namespace/prefix");
