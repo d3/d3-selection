600213e78b2fe8fb392a06d6a6a56f3186b4cc26
Mike Bostock <mbostock@gmail.com>
2015-05-11 13:01:59 -0700

    Expose select & selectAll in standalone.

--- standalone.js
+++ standalone.js
@@ -1,3 +1,5 @@
-var d3 = global.d3 || (global.d3 = {});
-d3.selection = require("./lib/d3/selection");
+var d3 = global.d3 || (global.d3 = {}),
+    selection = d3.selection = require("./lib/d3/selection");
+d3.select = selection.select;
+d3.selectAll = selection.selectAll;
 d3.namespace = {prefix: require("./lib/d3/namespace/prefix")};
