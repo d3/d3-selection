8517687528bf551224ef832a4afa1b8835046976
Mike Bostock <mbostock@gmail.com>
2015-05-11 14:17:52 -0700

    Add mouse, touch, touches. Fixes #2.

--- standalone.js
+++ standalone.js
@@ -3,3 +3,6 @@ var d3 = global.d3 || (global.d3 = {}),
 d3.select = selection.select;
 d3.selectAll = selection.selectAll;
 d3.namespace = require("./lib/d3/namespace/prefix");
+d3.mouse = require("./lib/d3/event/mouse");
+d3.touch = require("./lib/d3/event/touch");
+d3.touches = require("./lib/d3/event/touches");
