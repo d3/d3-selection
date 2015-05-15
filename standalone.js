7f00b74a23912804bfa422f3676ec7f22f49f77c
Mike Bostock <mbostock@gmail.com>
2015-05-15 16:50:11 -0700

    Restructure.

--- standalone.js
+++ standalone.js
@@ -1,8 +1,13 @@
-var d3 = global.d3 || (global.d3 = {}),
-    selection = d3.selection = require("./lib/d3/selection");
+var event = require("./lib/d3/event"),
+    namespace = require("./lib/d3/namespace"),
+    selection = require("./lib/d3/selection"),
+    d3 = global.d3 || (global.d3 = {});
+
+d3.mouse = event.mouse;
+d3.touch = event.touch;
+d3.touches = event.touches;
+d3.namespace = d3.ns = namespace;
+d3.selection = selection;
 d3.select = selection.select;
 d3.selectAll = selection.selectAll;
-d3.namespace = require("./lib/d3/namespace/prefix");
-d3.mouse = require("./lib/d3/event/mouse");
-d3.touch = require("./lib/d3/event/touch");
-d3.touches = require("./lib/d3/event/touches");
+selection.prototype.classed = selection.prototype.class;
