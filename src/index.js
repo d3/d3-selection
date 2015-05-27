f7ad6abd1b9eb4783ebbd73fbb9aad8360487c30
Rich Harris <richard.a.harris@gmail.com>
2015-05-27 17:30:56 -0400

    add ES6 module index and expose as jsnext:main

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import mouse from "./mouse";
+import namespace from "./namespace";
+import namespaces from "./namespaces";
+import requote from "./requote";
+import select from "./select";
+import selectAll from "./selectAll";
+import Selection from "./selection";
+import touch from "./touch";
+import touches from "./touches";
+
+export {
+  mouse,
+  namespace,
+  namespaces,
+  requote,
+  select,
+  selectAll,
+  Selection as selection,
+  touch,
+  touches
+};
