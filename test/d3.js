66d35cc8f9d19444feb0f8c181ae43eced15e2d5
Mike Bostock <mbostock@gmail.com>
2015-05-29 09:38:59 -0700

    Generate a custom build for tests.

---
+++ test/d3.js
@@ -0,0 +1,25 @@
+import {
+  event,
+  mouse,
+  namespace,
+  namespaces,
+  requote,
+  select,
+  selectAll,
+  selection,
+  touch,
+  touches
+} from "../index";
+
+export default {
+  get event() { return event; },
+  mouse: mouse,
+  namespace: namespace,
+  namespaces: namespaces,
+  requote: requote,
+  select: select,
+  selectAll: selectAll,
+  selection: selection,
+  touch: touch,
+  touches: touches
+};
