4866261bd0dc2c848974dfe81a001105a8d21517
Mike Bostock <mbostock@gmail.com>
2016-01-13 14:52:20 -0800

    Better namespace tests.

--- test/namespace-test.js
+++ test/namespace-test.js
@@ -1,16 +1,31 @@
 var tape = require("tape"),
     jsdom = require("jsdom"),
-    selection = require("../");
+    d3 = require("../");
 
-tape("selection.namespace returns the expected values for built-in namespaces", function(test) {
-  test.deepEqual(selection.namespace("svg:g"), {space: "http://www.w3.org/2000/svg", local: "g"});
-  test.deepEqual(selection.namespace("xhtml:b"), {space: "http://www.w3.org/1999/xhtml", local: "b"});
-  test.deepEqual(selection.namespace("xlink:href"), {space: "http://www.w3.org/1999/xlink", local: "href"});
-  test.deepEqual(selection.namespace("xml:lang"), {space: "http://www.w3.org/XML/1998/namespace", local: "lang"});
+tape("d3.namespace(name) returns name if there is no namespace prefix", function(test) {
+  test.equal(d3.namespace("foo"), "foo");
   test.end();
 });
 
-tape("selection.namespace(\"xmlns:…\") treats the whole name as the local name", function(test) {
-  test.deepEqual(selection.namespace("xmlns:xlink"), {space: "http://www.w3.org/2000/xmlns/", local: "xmlns:xlink"});
+tape("d3.namespace(name) coerces name to a string", function(test) {
+  test.equal(d3.namespace({toString: function() { return "foo"; }}), "foo");
+  test.deepEqual(d3.namespace({toString: function() { return "svg"; }}), {space: "http://www.w3.org/2000/svg", local: "svg"});
+  test.end();
+});
+
+tape("d3.namespace(name) returns the expected values for built-in namespaces", function(test) {
+  test.deepEqual(d3.namespace("svg"), {space: "http://www.w3.org/2000/svg", local: "svg"});
+  test.deepEqual(d3.namespace("xhtml"), {space: "http://www.w3.org/1999/xhtml", local: "xhtml"});
+  test.deepEqual(d3.namespace("xlink"), {space: "http://www.w3.org/1999/xlink", local: "xlink"});
+  test.deepEqual(d3.namespace("xml"), {space: "http://www.w3.org/XML/1998/namespace", local: "xml"});
+  test.deepEqual(d3.namespace("svg:g"), {space: "http://www.w3.org/2000/svg", local: "g"});
+  test.deepEqual(d3.namespace("xhtml:b"), {space: "http://www.w3.org/1999/xhtml", local: "b"});
+  test.deepEqual(d3.namespace("xlink:href"), {space: "http://www.w3.org/1999/xlink", local: "href"});
+  test.deepEqual(d3.namespace("xml:lang"), {space: "http://www.w3.org/XML/1998/namespace", local: "lang"});
+  test.end();
+});
+
+tape("d3.namespace(\"xmlns:…\") treats the whole name as the local name", function(test) {
+  test.deepEqual(d3.namespace("xmlns:xlink"), {space: "http://www.w3.org/2000/xmlns/", local: "xmlns:xlink"});
   test.end();
 });
