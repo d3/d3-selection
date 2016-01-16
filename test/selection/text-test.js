ef6003cfaa849001eab7b58d5fa906285a3b74b3
Mike Bostock <mbostock@gmail.com>
2016-01-16 13:39:45 -0800

    Remove space.

--- test/selection/text-test.js
+++ test/selection/text-test.js
@@ -3,7 +3,7 @@ var tape = require("tape"),
     d3 = require("../../");
 
 tape("selection.text returns text of the first element if no arguments are given", function(test) {
-  var document = jsdom.jsdom("<h1>first</h1> <h1>second</h1>"),
+  var document = jsdom.jsdom("<h1>first</h1><h1>second</h1>"),
       h = d3.select(document.body).selectAll("h1");
   test.equal(h.text(), "first");
   test.end();
@@ -20,7 +20,7 @@ tape("selection.text can take constant string argument", function(test) {
 });
 
 tape("selection.text can take a function argument and sets text according to return of function", function(test) {
-  var document = jsdom.jsdom("<h1>not set</h1> <h1>not set</h1>"),
+  var document = jsdom.jsdom("<h1>not set</h1><h1>not set</h1>"),
       h = d3.select(document.body).selectAll("h1").data([1, 3]);
   h.text(function(d) { return d; });
   var hNodesAfter = document.querySelectorAll("h1");
