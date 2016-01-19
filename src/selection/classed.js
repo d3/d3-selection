f5b68ab2d31114e4f95ff5f9525472f741d13ae5
Mike Bostock <mbostock@gmail.com>
2016-01-19 13:19:12 -0800

    Tests for selection.classed.
    Also rewrite selection.classed so that it’s more efficient in the case where
    multiple class names are specified, and so that the code is simpler.

--- src/selection/classed.js
+++ src/selection/classed.js
@@ -1,60 +1,75 @@
-import requote from "../requote";
-
-function collapse(string) {
-  return string.trim().replace(/\s+/g, " ");
+function classArray(string) {
+  return string.trim().split(/^|\s+/);
 }
 
-function classer(name) {
-  var re;
-  return function(node, value) {
-    if (classes = node.classList) return value ? classes.add(name) : classes.remove(name);
-    if (!re) re = classedRe(name); // Create only if classList is missing.
-    var classes = node.getAttribute("class") || "";
-    if (value) {
-      re.lastIndex = 0;
-      if (!re.test(classes)) node.setAttribute("class", collapse(classes + " " + name));
-    } else {
-      node.setAttribute("class", collapse(classes.replace(re, " ")));
+function classList(node) {
+  return node.classList || new ClassList(node);
+}
+
+function ClassList(node) {
+  this._node = node;
+  this._names = classArray(node.getAttribute("class") || "");
+}
+
+ClassList.prototype = {
+  add: function(name) {
+    var i = this._names.indexOf(name);
+    if (i < 0) {
+      this._names.push(name);
+      this._node.setAttribute("class", this._names.join(" "));
     }
+  },
+  remove: function(name) {
+    var i = this._names.indexOf(name);
+    if (i >= 0) {
+      this._names.splice(i, 1);
+      this._node.setAttribute("class", this._names.join(" "));
+    }
+  },
+  contains: function(name) {
+    return this._names.indexOf(name) >= 0;
+  }
+};
+
+function classedAdd(node, names) {
+  var list = classList(node), i = -1, n = names.length;
+  while (++i < n) list.add(names[i]);
+}
+
+function classedRemove(node, names) {
+  var list = classList(node), i = -1, n = names.length;
+  while (++i < n) list.remove(names[i]);
+}
+
+function classedTrue(names) {
+  return function() {
+    classedAdd(this, names);
   };
 }
 
-function classedRe(name) {
-  return new RegExp("(?:^|\\s+)" + requote(name) + "(?:\\s+|$)", "g");
-}
-
-function classedConstant(classers, value) {
+function classedFalse(names) {
   return function() {
-    var i = -1, n = classers.length;
-    while (++i < n) classers[i](this, value);
+    classedRemove(this, names);
   };
 }
 
-function classedFunction(classers, value) {
+function classedFunction(names, value) {
   return function() {
-    var i = -1, n = classers.length, v = value.apply(this, arguments);
-    while (++i < n) classers[i](this, v);
+    (value.apply(this, arguments) ? classedAdd : classedRemove)(this, names);
   };
 }
 
 export default function(name, value) {
-  var names = (name + "").trim().split(/^|\s+/);
+  var names = classArray(name + "");
 
   if (arguments.length < 2) {
-    var node = this.node(),
-        i = -1,
-        n = names.length,
-        classes = node.classList;
-    if (classes) { // SVG elements may not support DOMTokenList!
-      while (++i < n) if (!classes.contains(names[i])) return false;
-    } else {
-      classes = node.getAttribute("class") || "";
-      while (++i < n) if (!classedRe(names[i]).test(classes)) return false;
-    }
+    var list = classList(this.node()), i = -1, n = names.length;
+    while (++i < n) if (!list.contains(names[i])) return false;
     return true;
   }
 
   return this.each((typeof value === "function"
-      ? classedFunction
-      : classedConstant)(names.map(classer), value));
+      ? classedFunction : value
+      ? classedTrue
+      : classedFalse)(names, value));
 }
