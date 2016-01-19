14081693a999da6b38cd694ebc56fec033dc8746
Mike Bostock <mbostock@gmail.com>
2016-01-19 09:08:14 -0800

    Better select[All] tests.

--- test/select-test.js
+++ test/select-test.js
@@ -2,53 +2,55 @@ var tape = require("tape"),
     jsdom = require("jsdom"),
     d3 = require("../");
 
-tape("d3.select returns a selection", function(test) {
+tape("select(node) returns a selection", function(test) {
   var document = jsdom.jsdom("<h1>hello</h1>");
   test.ok(d3.select(document.body) instanceof d3.selection);
   test.end();
 });
 
-tape("d3.select can select by string", function(test) {
-  var document = global.document = jsdom.jsdom();
+tape("select(string) selects the first element that matches the selector string", function(test) {
+  var document = global.document = jsdom.jsdom("<h1 id='one'>foo</h1><h1 id='two'>bar</h1>");
   try {
-    test.deepEqual(d3.select("body"), {_nodes: [[document.body]], _parents: [null]});
+    test.deepEqual(d3.select("h1"), {_nodes: [[document.querySelector("h1")]], _parents: [null]});
     test.end();
   } finally {
     delete global.document;
   }
 });
 
-tape("d3.select can select an element", function(test) {
+tape("select(element) selects the given element", function(test) {
   var document = jsdom.jsdom("<h1>hello</h1>");
   test.deepEqual(d3.select(document.body), {_nodes: [[document.body]], _parents: [null]});
   test.end();
 });
 
-tape("d3.select can select a window", function(test) {
+tape("select(window) selects the given window", function(test) {
   var document = jsdom.jsdom("<h1>hello</h1>");
   test.deepEqual(d3.select(document.defaultView), {_nodes: [[document.defaultView]], _parents: [null]});
   test.end();
 });
 
-tape("d3.select can select a document", function(test) {
+tape("select(document) selects the given document", function(test) {
   var document = jsdom.jsdom("<h1>hello</h1>");
   test.deepEqual(d3.select(document), {_nodes: [[document]], _parents: [null]});
   test.end();
 });
 
-tape("d3.select can select a document element", function(test) {
+tape("select(documentElement) selects the given document element", function(test) {
   var document = jsdom.jsdom("<h1>hello</h1>");
   test.deepEqual(d3.select(document.documentElement), {_nodes: [[document.documentElement]], _parents: [null]});
   test.end();
 });
 
-tape("d3.select can select null", function(test) {
+tape("select(null) selects null", function(test) {
   var document = jsdom.jsdom("<h1>hello</h1>");
   test.deepEqual(d3.select(null), {_nodes: [[null]], _parents: [null]});
+  test.deepEqual(d3.select(undefined), {_nodes: [[undefined]], _parents: [null]});
+  test.deepEqual(d3.select(), {_nodes: [[undefined]], _parents: [null]});
   test.end();
 });
 
-tape("d3.select can select an arbitrary object", function(test) {
+tape("select(object) selects an arbitrary object", function(test) {
   var object = {};
   test.deepEqual(d3.select(object), {_nodes: [[object]], _parents: [null]});
   test.end();
--- test/selectAll-test.js
+++ test/selectAll-test.js
@@ -2,23 +2,23 @@ var tape = require("tape"),
     jsdom = require("jsdom"),
     d3 = require("../");
 
-tape("d3.selectAll returns a selection", function(test) {
+tape("selectAll(array) returns a selection", function(test) {
   var document = jsdom.jsdom("<h1>hello</h1>");
   test.ok(d3.selectAll([document.body]) instanceof d3.selection);
   test.end();
 });
 
-tape("d3.selectAll can select by string", function(test) {
-  var document = global.document = jsdom.jsdom();
+tape("selectAll(string) selects all elements that match the selector string, in order", function(test) {
+  var document = global.document = jsdom.jsdom("<h1 id='one'>foo</h1><h1 id='two'>bar</h1>");
   try {
-    test.deepEqual(d3.selectAll("body"), {_nodes: [document.querySelectorAll("body")], _parents: [null]});
+    test.deepEqual(d3.selectAll("h1"), {_nodes: [document.querySelectorAll("h1")], _parents: [null]});
     test.end();
   } finally {
     delete global.document;
   }
 });
 
-tape("d3.selectAll can select an array of elements", function(test) {
+tape("selectAll(array) selects an array of elements", function(test) {
   var document = jsdom.jsdom("<h1>hello</h1><h2>world</h2>"),
       h1 = document.querySelector("h1"),
       h2 = document.querySelector("h2");
@@ -26,27 +26,25 @@ tape("d3.selectAll can select an array of elements", function(test) {
   test.end();
 });
 
-tape("d3.selectAll can select a NodeList of elements", function(test) {
-  var document = jsdom.jsdom("<h1>hello</h1><h2>world</h2>"),
-      h1 = document.querySelector("h1"),
-      h2 = document.querySelector("h2");
+tape("selectAll(nodeList) selects a NodeList of elements", function(test) {
+  var document = jsdom.jsdom("<h1>hello</h1><h2>world</h2>");
   test.deepEqual(d3.selectAll(document.querySelectorAll("h1,h2")), {_nodes: [document.querySelectorAll("h1,h2")], _parents: [null]});
   test.end();
 });
 
-tape("d3.selectAll can select an empty array", function(test) {
+tape("selectAll(array) can select an empty array", function(test) {
   test.deepEqual(d3.selectAll([]), {_nodes: [[]], _parents: [null]});
   test.end();
 });
 
-tape("d3.selectAll can select an array that contains null", function(test) {
+tape("selectAll(array) can select an array that contains null", function(test) {
   var document = jsdom.jsdom("<h1>hello</h1><h2>world</h2>"),
       h1 = document.querySelector("h1");
   test.deepEqual(d3.selectAll([null, h1, null]), {_nodes: [[null, h1, null]], _parents: [null]});
   test.end();
 });
 
-tape("d3.selectAll can select an array that contains arbitrary objects", function(test) {
+tape("selectAll(array) can select an array that contains arbitrary objects", function(test) {
   var object = {};
   test.deepEqual(d3.selectAll([object]), {_nodes: [[object]], _parents: [null]});
   test.end();
