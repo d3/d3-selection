5cc2087f8a553d84b8a028183e2247d7127f64d4
Mike Bostock <mbostock@gmail.com>
2016-02-18 21:33:28 -0800

    More concise.

--- src/selection/merge.js
+++ src/selection/merge.js
@@ -5,7 +5,7 @@ export default function(selection) {
 
   for (var groups0 = this._groups, groups1 = selection._groups, m = groups0.length, merges = new Array(m), j = 0; j < m; ++j) {
     for (var group0 = groups0[j], group1 = groups1[j], n = group0.length, merge = merges[j] = new Array(n), node, i = 0; i < n; ++i) {
-      if ((node = group0[i]) || (node = group1[i])) {
+      if (node = group0[i] || group1[i]) {
         merge[i] = node;
       }
     }
