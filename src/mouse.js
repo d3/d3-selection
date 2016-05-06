23dc35203e06039948c471833b7429f025c4392f
Mike Bostock <mbostock@gmail.com>
2016-05-06 12:11:53 -0700

    Fix #71 - remove undocumented event argument.

--- src/mouse.js
+++ src/mouse.js
@@ -1,8 +1,8 @@
 import sourceEvent from "./sourceEvent";
 import point from "./point";
 
-export default function(node, event) {
-  if (event == null) event = sourceEvent();
+export default function(node) {
+  var event = sourceEvent();
   if (event.changedTouches) event = event.changedTouches[0];
   return point(node, event);
 }
