00f3a442d5b1277419d5b2c273c65fb1c78570ab
Mike Bostock <mbostock@gmail.com>
2018-03-05 09:44:54 -0800

    Generate ES bundles.

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,68 @@
+import uglify from "rollup-plugin-uglify";
+
 const definition = require("./package.json");
 const dependencies = Object.keys(definition.dependencies || {});
+const globals = dependencies.reduce((p, v) => (p[v] = "d3", p), {});
 
-export default {
-  input: "index",
-  external: dependencies,
-  output: {
-    extend: true,
-    file: `dist/${definition.name}.js`,
-    format: "umd",
-    globals: dependencies.reduce((p, v) => (p[v] = "d3", p), {}),
-    name: "d3"
+function onwarn(warning) {
+  if (warning.code !== "CIRCULAR_DEPENDENCY") {
+    console.error(`(!) ${warning.message}`);
   }
-};
+}
+
+const preamble = `// ${
+  definition.homepage || definition.name} Version ${
+  definition.version}. Copyright ${
+  (new Date).getFullYear()} ${
+  definition.author.name + (/\.$/.test(definition.author.name) ? "" : ".")}`;
+
+export default [
+  {
+    input: "index",
+    external: dependencies,
+    onwarn,
+    output: {
+      banner: preamble,
+      extend: true,
+      file: `dist/${definition.name}.js`,
+      format: "umd",
+      globals,
+      name: "d3"
+    }
+  },
+  {
+    input: "index",
+    external: dependencies,
+    plugins: [uglify({output: {preamble}})],
+    onwarn,
+    output: {
+      banner: preamble,
+      extend: true,
+      file: `dist/${definition.name}.min.js`,
+      format: "umd",
+      globals,
+      name: "d3"
+    }
+  },
+  {
+    input: "index",
+    external: dependencies,
+    onwarn,
+    output: {
+      banner: preamble,
+      file: `dist/${definition.name}.mjs`,
+      format: "es"
+    }
+  },
+  {
+    input: "index",
+    external: dependencies,
+    plugins: [uglify({output: {preamble}})],
+    onwarn,
+    output: {
+      banner: preamble,
+      file: `dist/${definition.name}.min.mjs`,
+      format: "es"
+    }
+  }
+];
