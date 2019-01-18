57373d42f334f854eb8752387faea7e4321adfad
Mike Bostock <mbostock@gmail.com>
2019-01-18 14:31:37 -0800

    Shorten.

--- src/selection/join.js
+++ src/selection/join.js
@@ -1,20 +1,7 @@
-import identity from "../identity";
-
-function joinAppend(name) {
-  return function(selection) {
-    return selection.append(name);
-  };
-}
-
-function joinRemove(selection) {
-  return selection.remove();
-}
-
 export default function(onenter, onupdate, onexit) {
-  if (typeof onenter !== "function") onenter = joinAppend(onenter + "");
-  if (onupdate == null) onupdate = identity;
-  if (onexit == null) onexit = joinRemove;
-  var enter = onenter(this.enter()), update = onupdate(this);
-  onexit(this.exit());
+  var enter = this.enter(), update = this, exit = this.exit();
+  enter = typeof onenter === "function" ? onenter(enter) : enter.append(onenter + "");
+  if (onupdate != null) update = onupdate(update);
+  if (onexit == null) exit.remove(); else onexit(exit);
   return enter && update ? enter.merge(update).order() : update;
 }
