4c599e6e4f076695f001e52e473786bc14956d0f
Riceball LEE <snowyu.lee@gmail.com>
2019-05-31 18:26:52 +0800

    perf(each): improve performance

--- src/selection/each.js
+++ src/selection/each.js
@@ -1,13 +1,15 @@
 export default function(callback) {
   var result = [];
+  var hasAsync;
   for (var groups = this._groups, j = 0, m = groups.length; j < m; ++j) {
     for (var group = groups[j], i = 0, n = group.length, node; i < n; ++i) {
       if (node = group[i]) {
-        result.push(callback.call(node, node.__data__, i, group));
+        var val = callback.call(node, node.__data__, i, group);
+        if (!hasAsync) hasAsync = val && typeof val.then === 'function';
+        result.push(val);
       }
     }
   }
 
-  var hasAsync = result.filter(item => item && typeof item.then === 'function').length;
   return hasAsync ? Promise.all(result) : this;
 }
