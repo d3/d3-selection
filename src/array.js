8cde069ccc9915aed0662f2ccd3e5fcae9c8f790
Mike Bostock <mbostock@gmail.com>
2019-11-16 15:06:47 -0800

    Correctly iterate over a string.

--- src/array.js
+++ src/array.js
@@ -1,3 +1,5 @@
 export default function(x) {
-  return x == null ? [] : "length" in x ? x : Array.from(x);
+  return x == null ? [] // null, undefined
+    : typeof x === "object" && "length" in x ? x // Array, TypedArray, NodeList
+    : Array.from(x); // Map, Set, iterable, string, or anything else
 }
