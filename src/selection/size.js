255cf5ccd63785e39a5afccf791079dcfcd89d40
Boyan Mihaylov <boyan.mihaylov@gtmhub.com>
2020-12-21 10:26:30 +0200

    Change usage of const in size.js as it throws on IE11 (fixes #272)

--- src/selection/size.js
+++ src/selection/size.js
@@ -1,5 +1,5 @@
 export default function() {
-  let size = 0;
-  for (const node of this) ++size; // eslint-disable-line no-unused-vars
+  var size = 0;
+  for (var node of this) ++size; // eslint-disable-line no-unused-vars
   return size;
 }
