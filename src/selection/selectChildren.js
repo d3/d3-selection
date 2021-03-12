e269effa81492a8e38e6cdba11bb9fbb7aaca2eb
xufanglu <3146974+likev@users.noreply.github.com>
2021-03-12 18:56:45 +0800

    Update selectChildren.js
    The ParentNode property `children` is a read-only property that returns a live HTMLCollection which contains all of the child elements of the node upon which it was called.
    https://developer.mozilla.org/en-US/docs/Web/API/ParentNode/children
    
    `d3.select("svg").selectChildren().remove()` will not success.

--- src/selection/selectChildren.js
+++ src/selection/selectChildren.js
@@ -3,7 +3,7 @@ import {childMatcher} from "../matcher.js";
 var filter = Array.prototype.filter;
 
 function children() {
-  return this.children;
+  return [...this.children];
 }
 
 function childrenFilter(match) {
