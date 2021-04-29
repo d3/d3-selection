712005a63384a6fe5831d024a144f78aa907cbb3
Philippe Rivière <fil@rezo.net>
2021-04-29 22:02:02 +0200

    Update test/jsdom.js
    Co-authored-by: Aulon Mujaj <aulon.mujaj@gmail.com>

--- test/jsdom.js
+++ test/jsdom.js
@@ -1,6 +1,6 @@
 import * as jsdom from "jsdom";
 
 export default function(html) {
-  return (new jsdom.JSDOM(html)).window.document;
+  const dom = new jsdom.JSDOM(html);
+  global.document = dom.window.document
 }
-
