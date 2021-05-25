492815872ddd18517961e924677772a2ed057b9a
Curran Kelleher <68416+curran@users.noreply.github.com>
2021-05-25 12:21:53 -0400

    Alternative solution for #257

--- src/selection/join.js
+++ src/selection/join.js
@@ -3,5 +3,5 @@ export default function(onenter, onupdate, onexit) {
   enter = typeof onenter === "function" ? onenter(enter) : enter.append(onenter + "");
   if (onupdate != null) update = onupdate(update);
   if (onexit == null) exit.remove(); else onexit(exit);
-  return enter && update ? enter.merge(update).order() : update;
+  return enter && update ? enter.selection().merge(update.selection()).order() : update;
 }
