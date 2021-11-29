ab1060e864f73a50a782f61092298137062e7f89
Mike Bostock <mbostock@gmail.com>
2021-11-29 13:01:04 -0800

    insertBefore, insertAfter

--- src/selection/insert.js
+++ src/selection/insert.js
@@ -5,10 +5,26 @@ function constantNull() {
   return null;
 }
 
+function selectThis() {
+  return this;
+}
+
+function selectNextSibling() {
+  return this.nextSibling;
+}
+
 export default function(name, before) {
   var create = typeof name === "function" ? name : creator(name),
       select = before == null ? constantNull : typeof before === "function" ? before : selector(before);
   return this.select(function() {
     return this.insertBefore(create.apply(this, arguments), select.apply(this, arguments) || null);
   });
+}
+
+export function insertBefore(name) {
+  return this.insert(name, selectThis);
+}
+
+export function insertAfter(name) {
+  return this.insert(name, selectNextSibling);
 }
